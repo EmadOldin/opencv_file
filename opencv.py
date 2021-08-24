864cddf4f67bad6ff633a9e21cae7a4fda0da26c
EmadOldin <87482212+EmadOldin@users.noreply.github.com>
2021-08-24 02:22:27 -0700

    Add files via upload

---
+++ opencv.py
@@ -0,0 +1,50 @@
+import cv2
+# img = cv2.imread('opencv2\\GettyImages-1232163930.jpg')
+
+thres = 0.5 #Threshold to detect object
+
+#setting of webcam
+cap = cv2.VideoCapture(0)
+
+cap.set(3, 640)
+cap.set(4, 480)
+
+
+classNames = []
+classFile = 'opencv2\\coco.names'
+with open(classFile , 'rt') as f:
+    classNames = f.read().rstrip('\n').split('\n')
+
+configPath = 'opencv2\\ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt'
+weightsPath = 'opencv2\\frozen_inference_graph.pb'
+
+net = cv2.dnn_DetectionModel(weightsPath,configPath)
+net.setInputSize(320,320)
+net.setInputScale(1.0/ 127.5)
+net.setInputMean((127.5, 127.5, 127.5))
+net.setInputSwapRB(True)
+
+
+while True:
+
+    success,img = cap.read()
+    classIds, confs, bbox = net.detect(img,confThreshold=thres)
+
+    print(classIds , bbox)
+
+    if len(classIds) != 0:
+
+        for clasId,confidence,box in zip(classIds.flatten(),confs.flatten(),bbox):
+            cv2.rectangle(img,box,color=(0,255,0),thickness=4)
+            cv2.putText(img,classNames[clasId-1].upper(),(box[0]+10,box[1]+30),
+            cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
+
+            cv2.putText(img,str(round(confidence*100,2)),(box[0]+200,box[1]+30),
+            cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
+
+
+    cv2.imshow("RONALDO FACE" , img)
+    # cv2.waitKey(0)
+    cv2.waitKey(1)
+
+
